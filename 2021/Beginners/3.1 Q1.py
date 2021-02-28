752bfa5056e2432f4adb70b0c2aaff93738404ab
Jboyblastmk3 <79814705+Jboyblastmk3@users.noreply.github.com>
2021-03-01 10:50:43 +1100

    3.1 Q1.py

---
+++ 2021/Beginners/3.1 Q1.py
@@ -0,0 +1,9 @@
+word = input('Treat?' + ' ')
+if word == 'Pastry':
+  print('Buttery flaky goodness')
+elif word == 'Cake':
+  print('A light and fluffy classic')
+elif word == 'Cookie':
+  print('Warm and crispy on the edges')
+else:
+  print('We do not sell that. It sounds yum!')
